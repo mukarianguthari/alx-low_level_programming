ec19301f10a0e3e257d8845e1f28ceb48cd537ba
Mukaria Nguthari <mukarianguthari@gmail.com>
2023-06-27 12:51:09 -0700

    correct betty error and documentation

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- *
  * _puts - prints a string, followed by a new line, to stdout
- *
+ * @str: string to put
  */
 
 void _puts(char *str)
 {
-	while(*str != '\0')
+	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
