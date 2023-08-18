3c3f3cd9787de2acbe379d34e3a96e223c142274
Mukaria Nguthari <mukarianguthari@gmail.com>
2023-08-18 05:54:23 -0700

    program that gets current process id

---
+++ shell_prac/pid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - PID
+ *
+ * Return: Always o.
+ *
+ */
+
+int main(void)
+{
+	pid_t my_pid;
+
+	my_pid = getpid();
+	printf("%u\n", my_pid);
+	return (0);
+}
+
